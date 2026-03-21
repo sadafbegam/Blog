9047fa3bde98d59bada8459ff6ebfbf0e5154d8a
Sadifbanu02 <21nn1a05i0.sadaf@gmail.com>
2026-03-21 12:48:22 +0530

    commit message

---
+++ blog-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css';
+import BlogHome from './Components/Blog/BlogHome';
+function App() {
+  return (
+    <>
+      <BlogHome />
+      </>
+  );
+}
+
+export default App;
